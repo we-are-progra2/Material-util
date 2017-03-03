f98b0454f42617ad13d30aa045cb510642762e40
m0rthal <jesusolmedo77@gmail.com>
2017-03-03 01:22:07 -0400

    Agregando Scripts para juegos plataformas Unity

---
+++ Varios/Scripts C#/Juego plataformas/_Scripts/monedasTrack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class monedasTrack : MonoBehaviour {
+
+    static Text scoreText;
+    static int Score = 0;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        scoreText = GameObject.FindGameObjectWithTag("GUImoneda").GetComponent<Text>();
+        UpdateScore(Score);
+	}
+
+
+    public static void UpdateScore(int Valor)
+    {
+        Score += Valor;
+        scoreText.text = "" + Score;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
