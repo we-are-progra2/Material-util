328608fb1bdb5869882f884229a77534789a5b12
SalinasLuis <luisfernando.salinas@unet.edu.ve>
2017-03-05 11:47:03 -0400

    codigo sin completar de la practica

---
+++ Codigos Java/ejercicio1/src/practicaEjerc1/Entrada.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaEjerc1;
+
+/**
+ *
+ * @author luisfernando
+ */
+public interface Entrada {
+    
+     String datos[] = {"5","comenzaron las practicas :):)","(:( )",
+                       "hoy no hay hamburguesa en el\n" + "comedor (:()",")(",
+                       ":)("};
+    
+}
---
+++ Codigos Java/ejercicio1/src/practicaEjerc1/Principal.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaEjerc1;
+
+/**
+ *
+ * @author luisfernando
+ */
+public class Principal implements Entrada{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       /* for (int i = 1; i <= Integer.parseInt(datos[0]); i++) {
+            
+            if(equilibrado(datos[i])) {
+                
+                System.out.println("Caso #"+i+": SI");
+                
+            }else {
+                
+                System.out.println("Caso #"+i+": NO");
+                
+            }
+            
+        }*/
+       
+       if(equilibrado(datos[2])) {
+                
+                System.out.println("Caso #"+3+": SI");
+                
+            }else {
+                
+                System.out.println("Caso #"+3+": NO");
+                
+            }
+        
+    }
+    public static boolean equilibrado(String mensaje) {
+        
+        boolean bandera = false;
+        
+        if(mensaje.equals("")) {
+            
+            return true;
+            
+        }else {
+            
+            for (int i = 0; i < mensaje.length(); i++) {
+                
+                if(mensaje.charAt(i) == ' ' || mensaje.charAt(i) == ':') {
+                    
+                    bandera = true;
+                    
+                }
+                
+            }
+            if(!bandera) {
+                
+                return false;
+                
+            }
+            for (int i = 0; i < mensaje.length(); i++) {
+                
+                if(mensaje.charAt(i) == '(' && i != 0) {
+                    
+                    if(mensaje.charAt(i-1) != ':') {
+                        
+                        if(cerrado(mensaje, i+1)) {
+                        
+                            bandera = true;
+                        
+                        }else {
+                        
+                            return false;
+                        
+                        }
+                    }
+                }
+            }
+            for (int i = mensaje.length()-1; i > 0; i--) {
+                
+                if(mensaje.charAt(i) == ')' && mensaje.charAt(i-1) != ':') {
+                    
+                    if(abierto(mensaje, i-1)) {
+                        
+                        bandera = true;
+                        
+                    }else {
+                        
+                        return false;
+                        
+                    }
+                }
+            }
+            
+            for (int i = 0; i < mensaje.length(); i++) {
+                
+                if(mensaje.charAt(i) == ':') {
+                    
+                    if(mensaje.charAt(i+1) == '(' || mensaje.charAt(i+1) == ')') {
+                        
+                        bandera = true;
+                        
+                    }
+                }
+            }
+            
+        }
+       return bandera; 
+    }
+    public static boolean cerrado(String mensaje, int indice) {
+        
+        for (int i = indice; i < mensaje.length(); i++) {
+            
+            if(mensaje.charAt(i) == ')') {
+                
+                if(mensaje.charAt(i-1) == ':') {
+                    break;
+                }
+                return true;
+                
+             }
+        }
+        return false;
+    }
+    public static boolean abierto(String mensaje, int indice) {
+        
+        for (int i = mensaje.length()-1; i > 0; i--) {
+            
+            if(mensaje.charAt(i) == '(') {
+                
+                if(mensaje.charAt(i-1) == ':') {
+                    
+                    break;
+                    
+                }
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    
+}
