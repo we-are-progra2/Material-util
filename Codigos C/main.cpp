780b08dee71ed1577805c26ec5f595e171c224da
Gustavo Andres Florez Ballesteros <dkmaster17@hotmail.com>
2017-03-07 00:05:08 -0400

    Add files via upload
    SJ _ apartir de los 20 elementos (platos) la recursividad se vuelve inmensa.

--- Codigos C/main.cpp
+++ Codigos C/main.cpp
@@ -50,7 +50,7 @@ Ejemplo01.open("Ejemplo01.txt");
 			if('('==Caracter_a_Caracter[i])//Con los parentesis de apertura y cierre correctamente puestos
 				{
 			
-			   for (j=i;j<strlen(Caracter_a_Caracter);j++)
+			   for (int j=i;j<strlen(Caracter_a_Caracter);j++)
 			   {
 			   		if ( ')'==Caracter_a_Caracter[j] )
 			   		{
@@ -91,10 +91,185 @@ Ejemplo01.open("Ejemplo01.txt");
 	}
 
 	}
+	
+	
+			
+				
+			
+		
+		
+		
+		
+int Intentando_Cumplir_La_Dieta( int Cantidad_de_Comidas,int Proteinas,int Carbohidratos,int Grasas,int **Posibilidades,int Pos_,int aux_P,int aux_C,int aux_G )	
+{
+	int band;
+	int aux_P2,aux_C2,aux_G2;	
+	bool t=false;
+	
+	if( Cantidad_de_Comidas<=Pos_ )//1<5 primer paso de la funcion.
+	{
+		
+	return -1;
+	cout<<"entro"<<endl;
+		
+	}
+	
+	
+	aux_P2=aux_P;
+	aux_C2=aux_C;
+	aux_G2=aux_G;	//0 en el primero,01 en el sgundo 012 en el tercerto
+	
+	for ( int i=Pos_;i<Cantidad_de_Comidas;i++ )
+	{
+	band=0;	
+	aux_P=Posibilidades[i][0]+aux_P2;//01 en el primer ciclo. 01+2 en el segundo .01+2+3 en el tercero...
+	aux_C=Posibilidades[i][1]+aux_C2;
+	aux_G=Posibilidades[i][2]+aux_G2;
+	
+	cout<<aux_P<<" "<<aux_C<<" "<<aux_G<<" "<<Pos_-1<<i<<endl;// nR DE COMbinacion
+	cin.get();
+	
+	
+	
+	if (aux_P==Proteinas && aux_C==Carbohidratos && aux_G==Grasas)
+	{
+		
+		return 1;
+		
+	}
+
+	
+	else
+	{
+		
+		band=Intentando_Cumplir_La_Dieta( Cantidad_de_Comidas,Proteinas, Carbohidratos,Grasas,Posibilidades,Pos_+1,aux_P,aux_C, aux_G );
+
+	}
+	if (band==1)
+	{
+		t=true;
+	}
+	
+	}
+	if (t)
+	{
+		return 1;
+	}
+	
+}
+
+
+
+void Problema02_Consumo_Alimenticio()
+{
+
+int Casos_de_Prueba,Proteinas, Carbohidratos,Grasas,Cantidad_de_Comidas;
+
+ifstream Ejemplo02;
+int band;
+
+
+int Caso_Actual=0;
+int aux_P,aux_C,aux_G;
+
+Ejemplo02.open("Ejemplo02.txt");
+
+	if ( Ejemplo02.fail() ) {
+
+		cout<<"error al abrir el archivo";
+
+	} else
+	{
+		Ejemplo02>>Casos_de_Prueba;
+		while ( !Ejemplo02.eof() )
+		{
+		band=0;
+		Caso_Actual++;	
+		Ejemplo02>>Proteinas>>Carbohidratos>>Grasas>>Cantidad_de_Comidas;
+		aux_P=aux_C=aux_G=0;
+		int** Posibilidades  =		new int *[Cantidad_de_Comidas];
+	
+		for(int i = 0; i < Cantidad_de_Comidas; ++i)
+    	{
+		Posibilidades[i] = new int[3];
+		}
+		cout<<Proteinas<<" "<<Carbohidratos<<" "<<Grasas<<endl;
+		cout<<Cantidad_de_Comidas<<endl;
+		
+		for(int i=0;i<Cantidad_de_Comidas;i++)
+		{
+			
+			for( int j=0;j<3;j++)
+			{
+				
+				Ejemplo02>>Posibilidades[i][j];
+				cout<<Posibilidades[i][j]<<" ";
+			}
+			cout <<endl;
+			
+		}
+		
+		for(int i=0;i<Cantidad_de_Comidas;i++)
+		{
+
+		aux_P=Posibilidades[i][0]+aux_P; // Para una suma consecutiva no funcional pero necesaria.
+		aux_C=Posibilidades[i][1]+aux_C;
+		aux_G=Posibilidades[i][2]+aux_G;
+
+			if( Posibilidades[i][0]==Carbohidratos && Posibilidades[i][1]==Proteinas && Posibilidades[i][2]==Grasas || ( aux_P==Proteinas && aux_G==Grasas && aux_C==Carbohidratos ))
+			{
+				
+			band=1;
+			break;	
+			
+			}
+		
+		}
+		
+		//------------------------------------------------
+		
+		if ( 1!=band )
+		{
+			
+		aux_P=Posibilidades[0][0]; //Enviando la primera linea de cada caso de prueba .
+		aux_C=Posibilidades[0][1];
+		aux_G=Posibilidades[0][2];
+			
+		band=Intentando_Cumplir_La_Dieta(Cantidad_de_Comidas,Proteinas,Carbohidratos,Grasas,Posibilidades,1,aux_P,aux_C,aux_G);
+				
+		}
+		
+		//-----------------------------------------------
+		
+		if ( 1==band )
+		{
+			cout<<endl;
+			cout<<"  \t Caso # "<<Caso_Actual<<" Si es posible"<<endl;
+			cout<<endl;
+			
+		}
+		else
+		{
+			cout<<endl;
+			cout<<"  \t Caso # "<<Caso_Actual<<" No es posible"<<endl;	
+			cout<<endl;
+		}
+		cin.get();
+		
+		}
+		
+	}
+	Ejemplo02.close();
+     
+	
+	
+}
+
 
 int main(int argc, char** argv) {
 
-	Problema01_Parentesis_Balanceados();
+	//Problema01_Parentesis_Balanceados();
+	Problema02_Consumo_Alimenticio();
 	return 0;
 	
 }
