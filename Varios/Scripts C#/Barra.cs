8a46f2f7ab3cfaa483d7defc6ae51d9a6843b47c
m0rthal <jesusolmedo77@gmail.com>
2017-03-30 21:42:40 -0400

    agregando script de la barra

---
+++ Varios/Scripts C#/Barra.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barra : MonoBehaviour {
+        public float velocidad = 150;
+        void Update () {
+                Touch toque;
+                for (var i = 0; i < Input.touchCount; ++i)
+                {
+                        toque = Input.GetTouch(i);
+
+                        // Get Horizontal Input
+                        if (toque.phase == TouchPhase.Began)
+                        {
+
+                                if (toque.position.x > Screen.width / 2) {
+                                        // Set Velocity (movement direction * speed)
+                                        GetComponent<Rigidbody2D>().velocity = Vector2.right * 1 * velocidad;
+                                }
+                                else if (toque.position.x <= Screen.width / 2) {
+                                        GetComponent<Rigidbody2D>().velocity = Vector2.left * 1 * velocidad;
+                                }
+                        } else if (toque.phase == TouchPhase.Ended) {
+                                GetComponent<Rigidbody2D>().velocity = Vector2.right * 0 * velocidad;
+                        }
+                }
+        }
+}
